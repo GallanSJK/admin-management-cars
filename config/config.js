c633412d10557139ba7045dc09c1281443cc7a90
Gallan Stefan Jaya Kelana <90021411+GallanSJK@users.noreply.github.com>
2023-05-15 20:48:23 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -17,11 +17,11 @@ module.exports = {
     port: "3306",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: "3306",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT,
   },
 };
